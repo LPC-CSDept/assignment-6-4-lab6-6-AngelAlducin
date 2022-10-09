5b6c83c2bc7caeda7905ba2587b78662defa02b1
AngelAlducin <aualducin@gamil.com>
2022-10-08 20:40:08 -0700

    Done

--- A64.cpp
+++ A64.cpp
@@ -15,6 +15,35 @@ using namespace std;
 //----------
 // Make a line for Function Prototype for getListprime Here
 //----------
+void printPrimeList(int a, int b){
+    int flag;
+    cout << " Prime numbers between " << a << " and " << b << ":" << endl;\
+    for(int i=a; i <=b; i++){  
+        if(i == 1 || i == 0)
+           continue;
+        flag =1;
+        for(int j =2; j<= (i/2); ++j){
+        // for(int j =2; j<= (i/j); ++j){
+			// for (int j=2; j<i; j++) 
+            if(i % j == 0){
+                flag =0;
+                break;
+            }
+        } 
+        if (flag == 1){
+            cout << i << " ";
+        }   
+    }
+}
+void getListprime(int begin){
+    printPrimeList(begin, 100);
+}
+void getListprime(){
+    printPrimeList(0,100);
+}
+void getListprime(int begin, int end){
+    printPrimeList(begin, end);
+}
 
 int main()
 {
@@ -27,9 +56,4 @@ int main()
 		getListprime();
 	else
 		getListprime(begin);
-}
-void getListprime(int begin, int end)
-{
-
-	// Complete this function
 }
